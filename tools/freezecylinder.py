a6972963c8da18348123ffd67c904486de1719bc
Svenn-Arne Dragly <s@dragly.com>
2014-10-25 00:27:16 +0200

    Updated cylinder.lmp with less dense fluid

--- tools/freezecylinder.py
+++ tools/freezecylinder.py
@@ -6,22 +6,30 @@ from sys import argv
 fileName = argv[1]
 lammpsHeader, atoms, system_lengths = load_atoms(fileName)
 
-cylinderRadius = 3.0
+cylinderRadius = system_lengths[1] * 0.4
 
 nFixed = 0
+free_count = 0
 for atom in atoms:
     diff = (atom["position"] - system_lengths / 2)
-    if(sqrt(diff[0]**2 + diff[1]**2) < cylinderRadius):
+    if(sqrt(diff[1]**2 + diff[2]**2) < cylinderRadius):
+        if free_count > len(atoms)/15:
+            atom["position"][0] = 1337.1
         atom["type"] = 18
-        atom["velocity"][0] = 0.0
+        atom["velocity"][0] = 2.0
         atom["velocity"][1] = 0.0
-        atom["velocity"][2] = 1.0
+        atom["velocity"][2] = 0.0
+        free_count += 1
     else:
-        atom["type"] = 0
+        atom["type"] = 1
         atom["velocity"][0] = 0.0
         atom["velocity"][1] = 0.0
         atom["velocity"][2] = 0.0
         nFixed += 1
+
+print "Atoms before delete: ", len(atoms)
+atoms = delete(atoms, where(atoms["position"][:,0] > 1337.0))
+print "Atoms after delete: ", len(atoms)
     
 print "Fixed", nFixed, "of", len(atoms), "atoms"
 save_atoms(lammpsHeader, atoms, argv[2])
